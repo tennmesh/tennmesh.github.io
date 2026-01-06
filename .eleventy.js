10171c1259fff16b51e753db0713c4a9bc61c5d4
tc2 <thecommschannel@proton.me>
2026-01-06 13:31:29 -0500

    Add CNAME file for custom domain tennmesh.com

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,9 @@ module.exports = function(eleventyConfig) {
   // Copy static assets
   eleventyConfig.addPassthroughCopy("src/assets");
 
+  // Copy CNAME file for custom domain
+  eleventyConfig.addPassthroughCopy("src/CNAME");
+
   // Watch CSS for changes
   eleventyConfig.addWatchTarget("src/assets/css/");
 
